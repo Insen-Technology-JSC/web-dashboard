9ca4f97992706b4dea808907c3510e403c6adad0
tuan.huynh <tuan.huynh@insentecs.com>
2025-11-11 11:30:12 +0700

    init project

---
+++ src/hooks.server.ts
@@ -0,0 +1,40 @@
+import type { Handle } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
+
+const isPublicPath = (pathname: string) => {
+    if (
+        pathname === '/login' || //login
+        pathname.startsWith('/authentication') ||
+        pathname.startsWith('/authentication/sign-in') ||
+        pathname.startsWith('/authentication/sign-up') ||
+        pathname.startsWith('/authentication/forgot-password') ||
+        pathname.startsWith('/api') ||
+        pathname.startsWith('/sitemap.xml') ||
+        pathname.startsWith('/svelte-kit') ||
+        pathname.startsWith('/favicon')
+    ) {
+        return true;
+    }
+    // static assets
+    if (/\.(css|js|mjs|map|png|jpg|jpeg|gif|svg|ico|webp|woff2?)$/i.test(pathname)) return true;
+    return false;
+};
+
+export const handle: Handle = async ({ event, resolve }) => {
+    const { url, cookies } = event;
+    const session = cookies.get('session');
+
+    // If not logged in and trying to access a non-public page, redirect to /login
+    if (!session && !isPublicPath(url.pathname)) {
+        throw redirect(303, '/authentication/sign-in');
+    }
+
+    // If logged in and trying to access /login, send to dashboard
+    if (session && url.pathname === '/authentication/sign-in') {
+        throw redirect(303, '/');
+    }
+
+    return resolve(event);
+};
+
+
